61b8c4aaef673d40e64a9e5a5c7d57cd5add2578
Ronal <ronal.sanx21@gmail.com>
2025-05-14 13:54:22 -0500

    Database connect

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const  config = {
+
+    "conexionURLDB":"mongodb://localhost:27017/task",
+}
--- src/db.js
+++ src/db.js
@@ -1,3 +1,17 @@
 import mongoose from "mongoose";
 
 
+export const connectDB = async () => {
+
+    try {
+    
+        const conn = await mongoose.connect('mongodb://localhost:27017/task');
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    
+    } catch (error) {
+    
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    
+    }
+}
